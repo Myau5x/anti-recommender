82b8fe142fc95310b62e3d581fcbd366230a8d7a
Olga Illarionova <ola.illari@gmail.com>
2018-11-27 12:16:10 -0800

    cleaning training data

--- train_model.py
+++ train_model.py
@@ -1,7 +1,23 @@
 import pyspark as ps
+from pyspark.ml.recommendation import ALS
+from pyspark.sql.types import *
+from pyspark.sql.functions import udf
+
 spark = (ps.sql.SparkSession.builder
         .master("local[4]")
         .appName("yelp_academic")
         .getOrCreate()
         )
 sc = spark.sparkContext
+
+def str_to_l(text):
+    return [word.strip() for word in text.split(',') ]
+
+def if_restaurant(text):
+    if text is None:
+        return False
+    else:
+        return 'Restaurants' in text
+
+if_rest_udf = udf(if_restaurant, BooleanType())
+str_to_list_udf = udf(str_to_l, ArrayType(StringType()))
