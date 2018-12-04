2b9f4f8899e46e4caaefb04139c8a3c192da5201
Olga Illarionova <ola.illari@gmail.com>
2018-12-03 17:30:06 -0800

    tune cl pred

--- trans_rat.py
+++ trans_rat.py
@@ -20,9 +20,42 @@ class CategoriesExpand:
 
     def transform(self,X, col='categories', inplace=False):
         X = X.copy() if not inplace else X
-        lbls = self.model_feature
+        lbls = self.model_features
         for lbl in lbls:
             X[lbl] = X[col].apply(lambda x: lbl in x)
         X['num_of_categ'] = X[col].map(lambda x: len(x.split(',')))
         X.drop(col, axis=1, inplace=True)
         return X
+#'categories': [{'alias': 'hawaiian', 'title': 'Hawaiian'},
+
+class CategoriesTransformScr:
+    """transform categories to dummy variables"""
+
+    def __init__(self, model_features):
+        self.model_features = model_features
+
+    def transform(self,X, col='categories', inplace=False):
+        X = X.copy() if not inplace else X
+        X[col] = X[col].map(lambda x:  list(map(lambda y : y['title'],x)))
+        lbls = self.model_features
+        for lbl in lbls:
+            X[lbl] = X[col].map(lambda x: lbl in x)
+        X['num_of_categ'] = X[col].map(lambda x: len(x))
+        X.drop(col, axis=1, inplace=True)
+        return X
+
+
+class RatingNormalizer:
+    def __init__(self, revCol = 'review_count', starCol = 'stars', n =250):
+        self.n = n
+        self.revCol = revCol
+        self.starCol = starCol
+
+    def transform(self, X, inplace = False):
+        X = X.copy() if not inplace else X
+        rev_norm = X[self.revCol].map(lambda x: x if x < self.n else self.n)
+        X['st_over_am'] = X[self.starCol]/rev_norm
+
+#Pipeline = Pipeline([FillPrice, CategoriesExpand, RatingNormalizer ])
+if (__name__ == "__main__"):
+    print('it is ok')
