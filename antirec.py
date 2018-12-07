ac52c4827042893b37d2552eb388041bdcd1d8a5
Olga Illarionova <ola.illari@gmail.com>
2018-12-06 23:26:55 -0800

    predict restaurants with known clusters

--- antirec.py
+++ antirec.py
@@ -16,12 +16,15 @@ with open('colPred213', 'rb') as f:
     colPred_pr = pickle.load(f)
 with open('thres_35_rf','rb') as f:
     th_rf35 = pickle.load(f )
+### for banned me
+with open('offline.d', 'rb') as f:
+    test_rest = pickle.load(f)
 rf = ComboModel('rforest/mrf_', th_rf35, colPred_pr, colClust_n)
 
 with open('model_feat.pkl', 'rb') as f:
     model_features = pickle.load(f)
 
-DEFAULT_USER = ['cl_t2',  'cl_t8', 'cl_t4']
+DEFAULT_USER = ['cl_t1',  'cl_t8', 'cl_t3']
 Real_user = []
 colShow = ['name', 'BAD', 'rating', 'location' , 'url']
 
@@ -35,26 +38,37 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    """Return a random prediction."""
+    """Return the prediction."""
     data = request.json
     loc = data['user_loc']
+    real_user = data['user_cl'].strip().split(' ')
+
+
     rests  = build_restoraunts(loc)
     if rests is None:
         return jsonify({'error': 'Cant get food'})
-    #prediction = model.predict_proba([data['user_input']])
+        #rests = test_rest
     else:
         X = cleaning_data(rests, model_features)
-        if len(Real_user) == 0:
+        if len(real_user) == 0:
             prediction = rf.predict(X, DEFAULT_USER)
         else:
-            prediction = rf.predict(X, Real_user)
+            prediction = rf.predict(X, real_user)
 
         am_bad = int(prediction.sum())
         am_norm = int(len(prediction) - am_bad)
         rests['url'] = rests['url'].str.split('?').map(lambda x : x[0])
         rests['BAD'] = prediction
+        bad = rests[prediction][colShow]
+        good = rests[~prediction][colShow]
+        return jsonify({"bad":bad.to_html(), "good":good.to_html()})
         #return jsonify({'bad restaurants': am_bad, 'norm restaurants': am_norm})
-        return rests[colShow].to_html()
+
+    #return jsonify({'cluster': real_user})
+@app.route('/predict2', methods=['GET', 'POST'])
+def predict2():
+    """Return a random prediction."""
+    return jsonify({'a':1, 'b':2})
 
 @app.route('/clusters', methods=['GET', 'POST'])
 def clusters():
@@ -66,4 +80,7 @@ def clusters():
     for x in l:
         user.append('cl_t'+str(int(x)))
     Real_user = list(set(user))
-    return jsonify({'cluster': Real_user})
+    s = ''
+    for cl in Real_user:
+        s += str(cl)+' '
+    return s
